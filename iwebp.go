fdf028f82d9477f0d83b69945c910f145d40fbad
Duckduckgot <49223254+Duckduckgot@users.noreply.github.com>
2024-10-02 21:37:54 +0900

    Add files via upload

---
+++ iwebp.go
@@ -0,0 +1,106 @@
+package webpbin
+
+import (
+	"errors"
+	"io"
+
+	"github.com/nickalie/go-binwrapper"
+)
+
+// GWebP compresses an image using the WebP format. Input format can be either PNG, JPEG, TIFF, WebP or raw Y'CbCr samples.
+// https://developers.google.com/speed/webp/docs/cwebp
+type IWebP struct {
+	*binwrapper.BinWrapper
+	input  io.Reader
+	input2 io.Reader
+	output io.Writer
+}
+
+// NewCWebP creates new GWebP instance.
+func NewGWebP(optionFuncs ...OptionFunc) *IWebP {
+	bin := &IWebP{
+		BinWrapper: createBinWrapper(optionFuncs...),
+	}
+	bin.ExecPath("gif2webp")
+
+	return bin
+}
+
+// Version returns GWebP version.
+func (c *IWebP) Version() (string, error) {
+	return version(c.BinWrapper)
+}
+
+// Input sets reader to convert.
+// InputFile or InputImage called before will be ignored.
+func (c *IWebP) Input(reader, reader2 io.Reader) *IWebP {
+	c.input = reader
+	c.input2 = reader2
+	return c
+}
+
+// Output specify writer to write webp file content.
+// OutputFile called before will be ignored.
+func (c *IWebP) Output(writer io.Writer) *IWebP {
+	c.output = writer
+	return c
+}
+
+func (c *IWebP) Run() error {
+	defer c.BinWrapper.Reset()
+
+	output, err := c.getOutput()
+
+	if err != nil {
+		return err
+	}
+
+	c.Arg("-o", output)
+
+	c.Arg("-loop", "0").Arg("-lossy", "")
+
+	err = c.setInput()
+
+	if err != nil {
+		return err
+	}
+
+	if c.output != nil {
+		c.SetStdOut(c.output)
+	}
+
+	err = c.BinWrapper.Run()
+
+	if err != nil {
+		return errors.New(err.Error() + ". " + string(c.StdErr()))
+	}
+
+	return nil
+}
+
+func (c *IWebP) setInput() error {
+	if c.input != nil {
+		c.Arg("-d").Arg("5000")
+		c.Arg("--").Arg("-")
+		c.StdIn(c.input)
+	} else {
+		return errors.New("Undefined input")
+	}
+	if c.input2 != nil {
+		c.Arg("-d").Arg("3000")
+		c.Arg("--").Arg("-")
+		c.StdIn(c.input2)
+	} else {
+		return errors.New("Undefined input")
+	}
+
+	return nil
+}
+
+func (c *IWebP) getOutput() (string, error) {
+	if c.output != nil {
+		return "-", nil
+	} else {
+		return "", errors.New("Undefined output")
+	}
+}
